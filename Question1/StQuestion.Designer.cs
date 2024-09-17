172b6483f74c15693450e17bb9f98bdf2a1de706
ElizaGitUser1 <lboyko2803@gmail>
2024-09-17 11:09:14 +0300

    first commit

---
+++ Question1/StQuestion.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Question1
+{
+    partial class StQuestion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btYes = new System.Windows.Forms.Button();
+            this.btNo = new System.Windows.Forms.Button();
+            this.lbQ = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btYes
+            // 
+            this.btYes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btYes.Location = new System.Drawing.Point(210, 168);
+            this.btYes.Name = "btYes";
+            this.btYes.Size = new System.Drawing.Size(75, 23);
+            this.btYes.TabIndex = 0;
+            this.btYes.Text = "Так";
+            this.btYes.UseVisualStyleBackColor = false;
+            this.btYes.Click += new System.EventHandler(this.btYes_Click);
+            // 
+            // btNo
+            // 
+            this.btNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btNo.Location = new System.Drawing.Point(460, 168);
+            this.btNo.Name = "btNo";
+            this.btNo.Size = new System.Drawing.Size(75, 23);
+            this.btNo.TabIndex = 1;
+            this.btNo.Text = "Ні";
+            this.btNo.UseVisualStyleBackColor = false;
+            this.btNo.Click += new System.EventHandler(this.btNo_Click);
+            this.btNo.MouseEnter += new System.EventHandler(this.btNo_MouseEnter);
+            // 
+            // lbQ
+            // 
+            this.lbQ.AutoSize = true;
+            this.lbQ.Location = new System.Drawing.Point(284, 104);
+            this.lbQ.Name = "lbQ";
+            this.lbQ.Size = new System.Drawing.Size(187, 16);
+            this.lbQ.TabIndex = 2;
+            this.lbQ.Text = "Ви задоволені стипендією?";
+            // 
+            // StQuestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbQ);
+            this.Controls.Add(this.btNo);
+            this.Controls.Add(this.btYes);
+            this.Name = "StQuestion";
+            this.Text = "Дайте відповідь на запитання";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btYes;
+        private System.Windows.Forms.Button btNo;
+        private System.Windows.Forms.Label lbQ;
+    }
+}
+
---
+++ Question1/StQuestion.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question1
+{
+    public partial class StQuestion : Form
+    {
+        private Timer moveButtonTimer; // Таймер для руху кнопки
+        private int moveStepX; // Крок переміщення по X
+        private int moveStepY; // Крок переміщення по Y
+        private Random random = new Random();
+        public StQuestion()
+        {
+            InitializeComponent();
+
+            // Налаштування таймера
+            moveButtonTimer = new Timer();
+            moveButtonTimer.Interval = 50; // Інтервал у мілісекундах
+            moveButtonTimer.Tick += MoveButtonTimer_Tick;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Обробник події натискання кнопки "Так"
+        private void btYes_Click(object sender, EventArgs e)
+         {
+            MessageBox.Show("Ми так і думали.");
+         }
+
+        // Обробник події наведення миші на кнопку "Ні"
+        private void btNo_MouseEnter(object sender, EventArgs e)
+        {
+            // Генерація випадкового напрямку руху
+            moveStepX = random.Next(-10, 11); // Рух по X (від -10 до 10)
+            moveStepY = random.Next(-10, 11); // Рух по Y (від -10 до 10)
+
+            // Запуск таймера для початку руху кнопки
+            moveButtonTimer.Start();
+        }
+
+        // Обробник події для таймера
+        private void MoveButtonTimer_Tick(object sender, EventArgs e)
+        {
+            // Отримуємо поточне місцезнаходження кнопки
+            Point currentLocation = btNo.Location;
+
+            // Обчислюємо нові координати
+            int newX = currentLocation.X + moveStepX;
+            int newY = currentLocation.Y + moveStepY;
+
+            // Перевіряємо, щоб кнопка не виходила за межі форми
+            if (newX < 0) newX = 0;
+            if (newY < 0) newY = 0;
+            if (newX + btNo.Width > this.ClientSize.Width)
+                newX = this.ClientSize.Width - btNo.Width;
+            if (newY + btNo.Height > this.ClientSize.Height)
+                newY = this.ClientSize.Height - btNo.Height;
+
+            // Переміщуємо кнопку на нове місце
+            btNo.Location = new Point(newX, newY);
+
+            // Зупиняємо таймер, якщо кнопка досягла краю
+            if (newX == 0 || newY == 0 || newX == this.ClientSize.Width - btNo.Width || newY == this.ClientSize.Height - btNo.Height)
+            {
+                moveButtonTimer.Stop();
+            }
+        }
+
+        private void btNo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ухти, спіймав!");
+        }
+    }
+}
